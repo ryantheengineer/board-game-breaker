ba17355ea75a9d2679b3705168b96b7aa5dc6202
ryantheengineer <ryan.larson.wa@gmail.com>
2022-09-26 16:02:29 -0600

    Dictionary based food counting

--- Wingspan/components.py
+++ Wingspan/components.py
@@ -35,7 +35,7 @@ class FoodDie:
 
 
 class Birdfeeder:
-    def __init__(self, die1=None, die2=None, die3=None, die4=None, die5=None, dice=None, foodinfeeder=None):
+    def __init__(self, die1=None, die2=None, die3=None, die4=None, die5=None, dice=None, foodinfeeder=None, foodoutfeeder=None):
         # "Invertebrate", "Fish", "Fruit", "Rodent", "Seed or Invertebrate"]
         self.die1 = FoodDie()
         self.die2 = FoodDie()
@@ -49,46 +49,54 @@ class Birdfeeder:
         self.dice = dice
 
         if foodinfeeder is None:
-            foodinfeeder = [0, 0, 0, 0, 0, 0]
-            for i in range(len(dice)):
-                if self.dice[i].infeeder is True:
-                    if self.dice[i].sideup == "Seed":
-                        foodinfeeder[0] += 1
-                    elif self.dice[i].sideup == "Invertebrate":
-                        foodinfeeder[1] += 1
-                    elif self.dice[i].sideup == "Fish":
-                        foodinfeeder[2] += 1
-                    elif self.dice[i].sideup == "Fruit":
-                        foodinfeeder[3] += 1
-                    elif self.dice[i].sideup == "Rodent":
-                        foodinfeeder[4] += 1
-                    elif self.dice[i].sideup == "Seed or Invertebrate":
-                        foodinfeeder[5] += 1
-                    else:
-                        print("There's a problem with the code")
-        self.foodinfeeder = foodinfeeder
+            _ = self.countinfeeder()
+            # foodinfeeder = {"Seed":0, "Invertebrate":0, "Fish":0, "Fruit":0, "Rodent":0, "Seed or Invertebrate":0}
+            # # foodinfeeder = [0, 0, 0, 0, 0, 0]
+            # # for i in range(len(dice)):
+            # #     if self.dice[i].infeeder is True:
+            # #         if self.dice[i].sideup == "Seed":
+            # #             foodinfeeder[0] += 1
+            # #         elif self.dice[i].sideup == "Invertebrate":
+            # #             foodinfeeder[1] += 1
+            # #         elif self.dice[i].sideup == "Fish":
+            # #             foodinfeeder[2] += 1
+            # #         elif self.dice[i].sideup == "Fruit":
+            # #             foodinfeeder[3] += 1
+            # #         elif self.dice[i].sideup == "Rodent":
+            # #             foodinfeeder[4] += 1
+            # #         elif self.dice[i].sideup == "Seed or Invertebrate":
+            # #             foodinfeeder[5] += 1
+            # #         else:
+            # #             print("There's a problem with the code")
+            # for i in range(len(dice)):
+            #     if self.dice[i].infeeder is True:
+            #         foodinfeeder[self.dice[i].sideup] += 1
+            #         self.foodinfeeder = foodinfeeder
 
     def countinfeeder(self):
         # Count the current food in the feeder (can be immediately after a roll
         # or just to check)
-        foodinfeeder = [0, 0, 0, 0, 0, 0]
-
+        # foodinfeeder = [0, 0, 0, 0, 0, 0]
+        foodinfeeder = {"Seed":0, "Invertebrate":0, "Fish":0, "Fruit":0, "Rodent":0, "Seed or Invertebrate":0}
         for i in range(len(self.dice)):
             if self.dice[i].infeeder is True:
-                if self.dice[i].sideup == "Seed":
-                    foodinfeeder[0] += 1
-                elif self.dice[i].sideup == "Invertebrate":
-                    foodinfeeder[1] += 1
-                elif self.dice[i].sideup == "Fish":
-                    foodinfeeder[2] += 1
-                elif self.dice[i].sideup == "Fruit":
-                    foodinfeeder[3] += 1
-                elif self.dice[i].sideup == "Rodent":
-                    foodinfeeder[4] += 1
-                elif self.dice[i].sideup == "Seed or Invertebrate":
-                    foodinfeeder[5] += 1
-                else:
-                    print("There's a problem with the code")
+                foodinfeeder[self.dice[i].sideup] += 1
+        # for i in range(len(self.dice)):
+        #     if self.dice[i].infeeder is True:
+        #         if self.dice[i].sideup == "Seed":
+        #             foodinfeeder[0] += 1
+        #         elif self.dice[i].sideup == "Invertebrate":
+        #             foodinfeeder[1] += 1
+        #         elif self.dice[i].sideup == "Fish":
+        #             foodinfeeder[2] += 1
+        #         elif self.dice[i].sideup == "Fruit":
+        #             foodinfeeder[3] += 1
+        #         elif self.dice[i].sideup == "Rodent":
+        #             foodinfeeder[4] += 1
+        #         elif self.dice[i].sideup == "Seed or Invertebrate":
+        #             foodinfeeder[5] += 1
+        #         else:
+        #             print("There's a problem with the code")
         self.foodinfeeder = foodinfeeder
 
         return foodinfeeder
@@ -96,24 +104,30 @@ class Birdfeeder:
     def countoutfeeder(self):
         # Count the current food outside the feeder (can be immediately after a
         # roll or just to check)
-        foodoutfeeder = [0, 0, 0, 0, 0, 0]
-
+        foodoutfeeder = {"Seed":0, "Invertebrate":0, "Fish":0, "Fruit":0, "Rodent":0, "Seed or Invertebrate":0}
+        
         for i in range(len(self.dice)):
             if self.dice[i].infeeder is False:
-                if self.dice[i].sideup == "Seed":
-                    foodoutfeeder[0] += 1
-                elif self.dice[i].sideup == "Invertebrate":
-                    foodoutfeeder[1] += 1
-                elif self.dice[i].sideup == "Fish":
-                    foodoutfeeder[2] += 1
-                elif self.dice[i].sideup == "Fruit":
-                    foodoutfeeder[3] += 1
-                elif self.dice[i].sideup == "Rodent":
-                    foodoutfeeder[4] += 1
-                elif self.dice[i].sideup == "Seed or Invertebrate":
-                    foodoutfeeder[5] += 1
-                else:
-                    print("There's a problem with the code")
+                foodoutfeeder[self.dice[i].sideup] += 1
+
+        # for i in range(len(self.dice)):
+        #     if self.dice[i].infeeder is False:
+        #         if self.dice[i].sideup == "Seed":
+        #             foodoutfeeder[0] += 1
+        #         elif self.dice[i].sideup == "Invertebrate":
+        #             foodoutfeeder[1] += 1
+        #         elif self.dice[i].sideup == "Fish":
+        #             foodoutfeeder[2] += 1
+        #         elif self.dice[i].sideup == "Fruit":
+        #             foodoutfeeder[3] += 1
+        #         elif self.dice[i].sideup == "Rodent":
+        #             foodoutfeeder[4] += 1
+        #         elif self.dice[i].sideup == "Seed or Invertebrate":
+        #             foodoutfeeder[5] += 1
+        #         else:
+        #             print("There's a problem with the code")
+        
+        self.foodoutfeeder = foodoutfeeder
 
         return foodoutfeeder
 
@@ -125,26 +139,13 @@ class Birdfeeder:
 
     def rolloutfeeder(self):
         # Roll only the dice that are out of the feeder
-        foodoutfeeder = [0, 0, 0, 0, 0, 0]
+        foodoutfeeder = {"Seed":0, "Invertebrate":0, "Fish":0, "Fruit":0, "Rodent":0, "Seed or Invertebrate":0}
 
         for i in range(len(self.dice)):
             if self.dice[i].infeeder is False:
                 self.dice[i].rollfood()
 
-                if self.dice[i].sideup == "Seed":
-                    foodoutfeeder[0] += 1
-                elif self.dice[i].sideup == "Invertebrate":
-                    foodoutfeeder[1] += 1
-                elif self.dice[i].sideup == "Fish":
-                    foodoutfeeder[2] += 1
-                elif self.dice[i].sideup == "Fruit":
-                    foodoutfeeder[3] += 1
-                elif self.dice[i].sideup == "Rodent":
-                    foodoutfeeder[4] += 1
-                elif self.dice[i].sideup == "Seed or Invertebrate":
-                    foodoutfeeder[5] += 1
-                else:
-                    print("There's a problem with the code")
+                foodoutfeeder = self.countoutfeeder()
 
         return foodoutfeeder
 
